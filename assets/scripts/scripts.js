6408211a324dbd300a42180defd923ff1665d0bd
David Lucio <david.a.lucio@gmail.com>
2021-06-23 12:17:05 -0700

    User choice loops completed, including error prevention.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,14 +1,57 @@
-var rps = ["rock", "paper", "scissors"];
+var rps = ["rock", "paper", "scissors"],
+    scoreboard=[0,0,0],
+    gamecounter=0;
 
 function initializegame(){
 
-    //Popup goes here
+    let gamecountermessage = (gamecounter == 0) ? "Shall we play a game?" : "Want to play again?";
 
-    // Choice resolution
-    var compchoice = choicerandomizer();
-    // console.log( `The computer chose ${rps[compchoice]}!` );
+    if( confirm(gamecountermessage) ){
+        gamecounter++;
+        playgame();
+    }
+    else{
+        alert("Okay! Thanks for coming!");
+        console.log("The only way to win is not to play.")
+    }
+}
 
-    //Result and reset
+function playgame(){
+
+    let userChoice,
+        validChoice = false;
+    
+    /** Get user input, only if valid **/
+    while(!validChoice){
+        userChoice = prompt("Choose Rock (r), Paper (p), or Scissors (s)...").toLowerCase();
+        switch(userChoice){
+            case "rock":
+            case "r":
+                validChoice=true;
+                userChoice=0;
+                console.log("User chose rock");
+                break;
+            case "paper":
+            case "p":
+                validChoice=true;
+                userChoice=1;
+                console.log("User chose paper");
+                break;
+            case "scissors":
+            case "s":
+                validChoice=true;
+                userChoice=2;
+                console.log("User chose scissors");
+                break;
+            default:
+                console.log("User is being a pain in the ass.");
+                alert("Please play by the rules.");
+                break;
+        }
+    }
+    
+    var compChoice = choicerandomizer();
+    // console.log( `The computer chose ${rps[compChoice]}!` );
 }
 
 function choicerandomizer(){
